26a3a2b2c619d3b2ba1a55cf767953314f8ea3a0
Nadika <nsbandara32@gmail.com>
2019-06-10 21:54:48 -0500

    Set the express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+// test endpoint
+app.get("/", (req, res) => {
+  res.send("API working!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
